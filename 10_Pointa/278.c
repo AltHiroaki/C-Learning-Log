c95b0f3bc19c7b526aaa9035b9c1d84c48bfaeb9
hirokun18 <hirokun0421@icloud.com>
2025-04-15 21:50:49 +0900

    Changes

---
+++ 10_Pointa/278.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    double x;
+    int a[3];
+    printf("nのアドレス:%p\n", &n);
+    printf("x以下略:%p\n", &x);
+    printf("a[0]のアドレス:%p\n", &a[0]);
+    printf("a[1]のアドレス:%p\n", &a[1]);
+    printf("a[3]のアドレス:%p\n", &a[3]);
+}
