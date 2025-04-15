8cd3daea4e24a4863649897c1d0833edbccc59df
hirokun18 <hirokun0421@icloud.com>
2025-04-15 21:53:00 +0900

    Changes

--- 10_Pointa/278.c
+++ 10_Pointa/278.c
@@ -2,10 +2,11 @@
 int main(void) {
     int n;
     double x;
-    int a[3];
+    int a[4];
     printf("nのアドレス:%p\n", &n);
     printf("x以下略:%p\n", &x);
     printf("a[0]のアドレス:%p\n", &a[0]);
     printf("a[1]のアドレス:%p\n", &a[1]);
     printf("a[3]のアドレス:%p\n", &a[3]);
+    printf("a[4]のアドレス:%p\n", &a[4]);
 }
